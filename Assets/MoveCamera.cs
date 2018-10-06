302062a5f3e6ed2d130bd2a97c0f62f1b8d34065
ajay <be.el.ajay@gmail.com>
2018-10-06 23:36:56 +0530

    Adding fog. Set to do next steps.

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    [SerializeField] float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var newPositionZOfCamera = transform.position.z + speed * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, transform.position.y, newPositionZOfCamera);
+    }
+}
---
+++ Assets/Scripts/TrackTileGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackTileGenerator : MonoBehaviour {
+
+    [SerializeField] GameObject firstTile;
+    [SerializeField] GameObject secondTile;
+    [SerializeField] GameObject tilePrefab;
+    [SerializeField] GameObject player;
+
+    private float tileDepth;
+
+    void Start()
+    {
+        tileDepth = firstTile.transform.position.z;
+    }
+
+    void Update()
+    {
+        print(player.transform.position.z + " ------ "+ firstTile.transform.position.z + tileDepth);
+        if (player.transform.position.z > firstTile.transform.position.z + tileDepth)
+        {
+            print("creating..");
+
+            var currentFirstTile = firstTile;
+            firstTile = secondTile;
+            Destroy(currentFirstTile);
+
+            var newTilePosition = new Vector3(firstTile.transform.position.x, firstTile.transform.position.y, firstTile.transform.position.z + 2 * tileDepth);
+            GameObject newTile = Instantiate(tilePrefab, newTilePosition, Quaternion.identity);
+            newTile.transform.SetParent(firstTile.transform.parent);
+
+            secondTile = newTile;
+        }
+    }
+}
